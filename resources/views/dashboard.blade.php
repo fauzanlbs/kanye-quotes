d053d377fae154bd045440e74bb3d5ee9a830929
Fauzan Lubis <34790560+fauzanlbs@users.noreply.github.com>
2024-09-03 23:16:30 +0800

    CU-86eq6q3z7 - Task 1 (#1)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <form action="{{ route('dashboard') }}" method="GET">
-                        <button type="submit">Refresh Quotes</button>
+                        <button type="submit">Refresh Quotes!</button>
                     </form>
                 </div>
             </div>
